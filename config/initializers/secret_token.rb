52c57bfcd2ec23ab7fb29cb160530d25fd58a8bb
Nothandok <thando.tk@gmail.com>
2016-03-14 20:48:45 +0200

    initial blog commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThandoBlog::Application.config.secret_token = '00ffde82c264835bde1879916201b8c56b0ad335a759657b03b905df7e683602e097371097001404bfe6bf211ae5759100128d1c14d0b2b4f237e4a4dc9cbf4a'
